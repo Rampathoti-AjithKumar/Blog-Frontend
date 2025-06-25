2cdf75f6e8d4605f7c9a85eca10a3092e68a62a1
Rampathoti Ajithkumar <rampathotiajithkumar9330@gmail.com>
2025-06-25 18:07:20 +0530

    Update LoginScreen.js

--- src/components/AuthScreens/LoginScreen.js
+++ src/components/AuthScreens/LoginScreen.js
@@ -14,7 +14,7 @@ const LoginScreen = () => {
 
     try {
       const { data } = await axios.post(
-        "/auth/login",
+        "https://blog-app-backend-liard.vercel.app/auth/login",
         { email, password }
       );
       localStorage.setItem("authToken", data.token);
